5f3ef8c89d994bbe703584fa544fcb424754f175
Lateef Quadri <Lateef9816@gmail.com>
2020-08-09 15:13:33 +0100

    Added cartIcon and cartDropdwon to our application with Cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { store, reducer, combineReducers } from "redux";
 import userReducer from "./user/user-reducer";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
